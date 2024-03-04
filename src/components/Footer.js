f874b2b09e59a8378964f9209defaa7ab8bd4a10
Amir Famili <44036070+AmirFamili@users.noreply.github.com>
2024-03-04 11:28:41 -0600

    get year for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
+
+
 const Footer = () => {
     return ( 
         <footer className="text-white py-10 px-10 bg-slate-800">
           
          <a href="#header" className='-mt-14 absolute text-2xl animate-bounce'> <i className="transition fa-solid fa-chevron-up text-white p-4 bg-slate-800 rounded-3xl hover:-translate-y-1 hover:scale-105"></i></a>
         
-          <p className=" text-center">© 2024 <a className="hover:text-yellow-main transition" href="#">THE AUTHENTIC APPROACH. LLC.</a><span className="text-sm ">  Designed & Managed by <a className="hover:text-yellow-main" target="_blank" href="http://amirfamili.com">AMF</a></span></p>
+          <p className=" text-center">© {(new Date().getFullYear())} <a className="hover:text-yellow-main transition" href="#">THE AUTHENTIC APPROACH. LLC.</a><span className="text-sm ">  Designed & Managed by <a className="hover:text-yellow-main" target="_blank" href="http://amirfamili.com">AMF</a></span></p>
           
         </footer>
      );
